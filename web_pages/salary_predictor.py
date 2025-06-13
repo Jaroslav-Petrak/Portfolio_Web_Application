1cadf1b78bb43a906913caafa5db44b43c387be1
Jaroslav Petrák <jaro.p.main@gmail.com>
2025-06-13 02:15:48 +0200

    Preparation of Salary Predictor page

--- web_pages/salary_predictor.py
+++ web_pages/salary_predictor.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import config.config
 
-st.title("Salary Predictor")
+### CONFIGURATION OF THE TITLE FONTS ###
+st.markdown(config.config.title_fonts, unsafe_allow_html=True)
+
+
+st.markdown(f'<div class="title-font">SALARY PREDICTOR</div>', unsafe_allow_html=True)
+st.markdown('<div class="subtitle-font">for Fair Compensation</div>', unsafe_allow_html=True)
+
+st.markdown("""
+        <hr style="
+            border: none;
+            height: 4px;
+            background-color: white;
+            margin: 10px 0;
+            box-shadow: none;
+            opacity: 1;
+        ">
+    """, unsafe_allow_html=True)
+

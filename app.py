a0cb7260b1936a9fb73311545786074530a03f97
Jaroslav Petrák <jaro.p.main@gmail.com>
2025-06-13 18:41:54 +0200

    Changed font color on the navigation panel to white, added "Contact" page

--- app.py
+++ app.py
@@ -32,13 +32,38 @@ st.markdown(f"""
     </style>
 """, unsafe_allow_html=True)
 
+### WHITE FONT OF TEXT IN NAVIGATION PANEL###
+st.markdown("""
+    <style>
+    section[data-testid="stSidebar"] * {
+        color: white !important;
+    }
+
+    section[data-testid="stSidebar"] a:hover {
+        color: white !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <style>
+    /* Selected navigation link background in sidebar */
+    section[data-testid="stSidebar"] a[aria-current="page"] {
+        background-color: #1f1f1f !important;
+        color: white !important;          /* keep text visible */
+        border-radius: 8px;               /* optional rounded corners */
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 
 
 main_page = st.Page(page="web_pages/main_page.py", title="Main Page")
 salary_predictor = st.Page(page="web_pages/salary_predictor.py", title="Salary Predictor")
+contact = st.Page(page="web_pages/contact.py", title="Contact")
 
 pg = st.navigation({
-    "NAVIGATION": [main_page],
+    "NAVIGATION": [main_page, contact],
     "Portfolio Projects": [salary_predictor]
 })
 
---
+++ web_pages/contact.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import config.config
+import base64
+
+st.markdown(config.config.title_fonts, unsafe_allow_html=True)
+st.markdown(f'<div class="title-font">CONTACT</div>', unsafe_allow_html=True)
+st.markdown('<div class="subtitle-font">Information</div>', unsafe_allow_html=True)
+
+### FIRST DIVISION LINE ###
+st.markdown("""
+        <hr style="
+            border: none;
+            height: 4px;
+            background-color: white;
+            margin: 10px 0;
+            box-shadow: none;
+            opacity: 1;
+        ">
+    """, unsafe_allow_html=True)
+### CREATION OF 2 SEPARATE ROWS FOR LINKED & EMAIL ###
+linkedin_icon_part, email_icon_part, filler_part = st.columns([2, 2, 14], gap="small")
+
+
+def img_to_base64(img_path):
+    with open(img_path, "rb") as img_file:
+        b64_string = base64.b64encode(img_file.read()).decode()
+    return b64_string
+
+### LINKEDIN PART ###
+with linkedin_icon_part:
+    linked_icon_base64 = img_to_base64("images/linkedin_icon.png")
+    html_code_linkedin_icon = f"""
+    <a href="https://www.linkedin.com/in/bc-jaroslav-petr%C3%A1k-7b9704264/" target="_blank" style="display:inline-block;">
+        <img src="data:image/png;base64,{linked_icon_base64}" width="100" style="vertical-align:middle;">
+    </a>
+    """
+    st.markdown(html_code_linkedin_icon, unsafe_allow_html=True)
+
+### EMAIL PART ###
+with email_icon_part:
+    email_icon_base64 = img_to_base64("images/email_icon.png")
+    html_code_email_icon = f"""
+    <a href="mailto:jaro.p.main@gmail.com" target="_blank" style="display:inline-block;">
+        <img src="data:image/png;base64,{email_icon_base64}" width="115" style="vertical-align:middle;">
+    </a>
+    """
+    st.markdown(html_code_email_icon, unsafe_allow_html=True)
